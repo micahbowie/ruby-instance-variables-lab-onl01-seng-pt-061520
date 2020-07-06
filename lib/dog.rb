ad6b9e78da33a59b530a79a3fbf69ac3535874e5
Micah Bowie <micahbowie20@gmail.com>
2020-07-06 09:23:25 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+class Dog
+  def name=(name)
+    @this_dogs_name = name
+  end 
+  
+  def name
+    @this_dogs_name
+  end 
+end 
+
+
+lassie = Dog.new
+lassie.name = "Lassie"
+ 
+puts lassie.name
+ 
